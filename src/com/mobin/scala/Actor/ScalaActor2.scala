4aeea9a459aab9cdd9ea0391b016c3f2f1fb3148
mobin <996833461@qq.com>
2016-03-19 00:00:16 +0800

    actor

--- src/com/mobin/scala/Actor/ScalaActor2.scala
+++ src/com/mobin/scala/Actor/ScalaActor2.scala
@@ -7,18 +7,24 @@ import scala.actors._
   */
 object ScalaActor2 extends  App{
 
-     object  hiactor extends  Actor{
-        def act(): Unit ={
-          receive{   //react使用后就会释放线程，若要一起启动线程可用loop
-            case 1 => println("one")
-            case 2 => println("two")
-            case 3 => println("three")
-            case 4 => react{
-              case 5 => println("four+five")
-            }}
-        }
+     object  hiactor extends  Actor {
+       def act(): Unit = {
+         loop {   //loopWhilw(条件)//条件为真时才执行括号内的，否则不执行
+         react {
+           //react使用后就会释放线程，若要一起启动线程可用loop
+           case 1 => println("one")
+           case 2 => println("two")
+           case 3 => println("three")
+           case 4 => react { case 5 => println("four+five")}
+         }
+         }
+       }
      }
+
   hiactor.start()
-  for(i <- 1 to 5)
-    hiactor ! i
+//  for(i <- 1 to 5)
+    hiactor ! 1
+    hiactor ! 4
+    hiactor ! 3
+    hiactor ! 5//把这行注释掉hiactor ! 3不会执行
 }
