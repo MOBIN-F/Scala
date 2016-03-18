d8812bc2bb5a8202a76545a8bb2bd3a95e1b04b3
mobin <996833461@qq.com>
2016-03-18 21:36:29 +0800

    Actor

--- src/com/mobin/scala/Actor/ScalaActor1.scala
+++ src/com/mobin/scala/Actor/ScalaActor1.scala
@@ -1,26 +1,29 @@
 package com.mobin.scala.Actor
-import  actors.Actor
+
+import actors.Actor
+
 /**
   * Created by Mobin on 2016/3/18.
   */
-object ScalaActor1 extends  App{
+object ScalaActor1 extends App {
 
-  class hiactor extends  Actor{
-    def act(){
-      while (true){
+  class hiactor extends Actor {
+    def act() {
+      while (true) {
         receive {
-          case   msg:String => println("hello  " + msg)
-        }
+          case msg: String => println("hello  " + msg) }
       }
     }
   }
 
-  def SHOW(m :String) = {
+  def SHOW(m: String) = {
     val ACTOR1 = new hiactor
     ACTOR1.start
     ACTOR1 ! m
-    ACTOR1 ! m + m}   //m+m的值总是在m之后输出
-    val scala = Array("spark","hadoop","storm")
-    for (i <- scala)
-      SHOW(i)
+    ACTOR1 ! m + m //m+m的值总是在m之后输出,相对于AKKA，m和m+m不是连在一起的
+  }
+
+  val scala = Array("spark", "hadoop", "storm")
+  for (i <- scala)
+    SHOW(i)
 }
