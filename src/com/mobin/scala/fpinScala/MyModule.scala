529205e6a594cc69fa7118f66f708b12057802ff
mobin <996833461@qq.com>
2016-06-03 20:49:08 +0800

    计算一个数的阶乘

--- src/com/mobin/scala/fpinScala/MyModule.scala
+++ src/com/mobin/scala/fpinScala/MyModule.scala
@@ -5,25 +5,27 @@ package com.mobin.scala.fpinScala
   */
 object MyModule {
 
-  def abs(n : Int) : Int =
-        if(n < 0) -n
-        else n
-  private def formatAbs(x : Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+  def abs(n: Int): Int =
+      if (n < 0) -n
+      else n
+
+
+  private def formatAbs(x: Int) = {
+     val msg = "The absolute value of %d is %d"
+     msg.format(x, abs(x))
   }
 
   //计算一个数的阶乘
-  def factorial(n :Int) ={
-      def go(n : Int,acc : Int) :Int=
-           if(n <= 0) acc
-           else go(n-1,n*acc)
+  def factorial(n: Int) = {
+     def go(n: Int, acc: Int): Int =
+        if (n <= 0) acc
+        else go(n - 1, n * acc)
 
-         go(n,1)
+       go(n, 1)
   }
 
   def main(args: Array[String]) {
-       println(formatAbs(-42))
-      println(factorial(7))
+    println(formatAbs(-42))
+    println(factorial(7))
   }
 }
