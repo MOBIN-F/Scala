42ee85bef3c16256cb3ea878aab54c02e3f49de4
mobin <996833461@qq.com>
2016-06-03 20:47:34 +0800

    《Scala函数式编程》的例子与练习

---
+++ src/com/mobin/scala/fpinScala/MyModule.scala
@@ -0,0 +1,29 @@
+package com.mobin.scala.fpinScala
+
+/**
+  * Created by Mobin on 2016/6/3.
+  */
+object MyModule {
+
+  def abs(n : Int) : Int =
+        if(n < 0) -n
+        else n
+  private def formatAbs(x : Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  //计算一个数的阶乘
+  def factorial(n :Int) ={
+      def go(n : Int,acc : Int) :Int=
+           if(n <= 0) acc
+           else go(n-1,n*acc)
+
+         go(n,1)
+  }
+
+  def main(args: Array[String]) {
+       println(formatAbs(-42))
+      println(factorial(7))
+  }
+}
