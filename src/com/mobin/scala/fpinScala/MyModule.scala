5c69c38ff61b78f8ea46060a53c4c523d5d77e04
mobin <996833461@qq.com>
2016-06-05 13:49:35 +0800

    高阶函数

--- src/com/mobin/scala/fpinScala/MyModule.scala
+++ src/com/mobin/scala/fpinScala/MyModule.scala
@@ -24,8 +24,17 @@ object MyModule {
        go(n, 1)
   }
 
+  //高阶函数
+  //参数f为一个函数，Int => Int为函数的参数类型，表示f接收一个整型参数并返回一个整型结果
+  def formatResult(name : String,n : Int,f : Int => Int)={
+      val msg = "The %s of %d is %d"
+       msg.format(name,n,f(n))
+  }
+
   def main(args: Array[String]) {
-    println(formatAbs(-42))
-    println(factorial(7))
+//    println(formatAbs(-42))
+//    println(factorial(7))
+    println(formatResult("absolute value",-42,abs))
+    println(formatResult("factorial",7,factorial))
   }
 }
