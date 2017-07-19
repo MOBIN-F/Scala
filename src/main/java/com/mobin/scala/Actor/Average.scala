6e8060fbf4281a3f2f8d0cbc4cdbe4f29163d243
mobin <996833461@qq.com>
2017-07-19 22:43:02 +0800

    《快学Scala》Actor章节练习1

---
+++ src/main/java/com/mobin/scala/Actor/Average.scala
@@ -0,0 +1,54 @@
+package com.mobin.scala.Actor
+
+import scala.util.Random
+import scala.actors.Actor
+
+/**
+  * Created by Mobin on 2017/7/19.
+  */
+object Average {
+
+  val n = 100
+  val nParts = 2
+  val partSize = n / nParts
+  var rands = rand(n)
+
+  def rand(n: Int) = new Array[Int](n).map(_ => Random.nextInt(n))
+
+  class AverageActor extends Actor {
+    val now = System.nanoTime()
+    var total = 0
+    var countParts = 0
+    def act: Unit = {
+      total += 1
+      countParts += 1
+      if (countParts < nParts){
+        act
+      }else{
+        println(total / nParts )
+        println("%d millseconds".format((System.nanoTime() - now)))
+      }
+    }
+  }
+
+  class SumActor(average: AverageActor) extends Actor{
+    def act: Unit ={
+      while (true){
+        receive{
+          case a: Array[Int] => {
+            average ! a.sum /partSize
+          }
+        }
+      }
+    }
+  }
+
+  def main(args: Array[String]) {
+    val avg = new AverageActor
+    avg.start()
+
+    (0 until nParts).foreach{
+      i => (new SumActor(avg)).start() ! rands.slice(i * partSize, (i +1) * partSize)
+    }
+  }
+}
