4ed575bc8b34aab2166795ccd54c9fdef3307171
mobin <996833461@qq.com>
2017-07-22 22:16:50 +0800

    单词统计

--- src/main/java/com/mobin/scala/Actor/WordCount.scala
+++ src/main/java/com/mobin/scala/Actor/WordCount.scala
@@ -3,6 +3,7 @@ package com.mobin.scala.Actor
 import java.io.File
 import scala.actors.Actor
 import scala.io.Source
+import scala.util.matching.Regex
 
 /**
   * Created by Mobin on 2017/7/20.
@@ -11,6 +12,8 @@ import scala.io.Source
   * 结果汇总到一起
   */
 object WordCount {
+  case class CountFileValue(n: Int)
+  case class TotalFiles(n: Int)
 
   def subDir(dir: File): Iterator[File] = {
     if (dir.isDirectory) {
@@ -20,37 +23,78 @@ object WordCount {
     children.toIterator ++ children.toIterator.flatMap(subDir _)
   }
 
-  class ErdogicDirActor() extends Actor{
+  class ErdogicDirActor(a:File => Unit,countActor: CountActor ) extends Actor{
+    var countFile = 0
+    var countDir = 1
     def act: Unit ={
       while (true){
         receive{
           case dir: File => {
             dir.listFiles.foreach{
               _ match {
-                case f: File if f.isDirectory => this!f
-                case f: File =>  new ReadFileActor().start() ! f
+                case f: File if f.isDirectory => countDir += 1;this!f
+                case f: File => countFile +=1; a(f)
               }
+            }
+            countDir -= 1
+            println(countDir)
+            if (countDir == 0) {
+              countActor! TotalFiles(countFile)
+              exit()
             }
           }
         }
       }
-
     }
   }
 
-  class ReadFileActor extends Actor{
+  class ReadFileActor(rex: Regex, countActor: CountActor) extends Actor{
     def act: Unit ={
        react{
          case file: File => {
-           val line = Source.fromFile(file).getLines()
-           println(file.getName)
+           val word = Source.fromFile(file).getLines().mkString
+           countActor ! CountFileValue(rex.findAllIn(word).size)
          }
        }
     }
   }
 
+  class CountActor extends Actor{
+    var count = 0
+    var totalFile = 0
+    var sum = 0
+    def  act: Unit ={
+        loop{
+          react{
+            case CountFileValue(n) => {
+              count += 1
+              sum += n
+              if (count == totalFile){
+                println("符合的单词数为：" + sum)
+                exit()
+              }
+            }
+            case TotalFiles(n) => {
+              totalFile = n
+              if (totalFile ==0)
+              exit()
+            }
+          }
+        }
+    }
+  }
+
   def main(args: Array[String]) {
     val dir = new File("E:\\Shell Script")
-    new ErdogicDirActor().start() ! dir
+    val rex = """mobin""".r
+
+    val countActor = new CountActor()
+    countActor.start()
+
+    //隐匿转换传入一个文件，返回一个Actor
+    new ErdogicDirActor(
+      (f: File) => new ReadFileActor(rex, countActor).start()!f,
+      countActor
+    ).start() ! dir
   }
 }
